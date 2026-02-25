49d09ba433641c762b8ca1c50089702f1441729d
Guilherme Caulada <guilherme.caulada@gmail.com>
2026-02-25 15:55:06 -0300

    feat(nvim): add smart-splits for seamless tmux/nvim navigation
    Add smart-splits.nvim plugin for C-h/j/k/l navigation between Neovim
    splits and tmux panes. Add A-h/j/k/l for resizing across both. Remove
    manual C-w based window navigation keymaps replaced by smart-splits.

--- home/dot_config/nvim/lua/exact_plugins/navigation.lua
+++ home/dot_config/nvim/lua/exact_plugins/navigation.lua
@@ -1,5 +1,21 @@
 return {
   {
+    'mrjones2014/smart-splits.nvim',
+    lazy = false,
+    config = function()
+      local ss = require('smart-splits')
+      ss.setup({ at_edge = 'wrap' })
+      vim.keymap.set('n', '<C-h>', ss.move_cursor_left, { desc = 'Focus left split/pane' })
+      vim.keymap.set('n', '<C-j>', ss.move_cursor_down, { desc = 'Focus lower split/pane' })
+      vim.keymap.set('n', '<C-k>', ss.move_cursor_up, { desc = 'Focus upper split/pane' })
+      vim.keymap.set('n', '<C-l>', ss.move_cursor_right, { desc = 'Focus right split/pane' })
+      vim.keymap.set('n', '<A-h>', ss.resize_left, { desc = 'Resize left' })
+      vim.keymap.set('n', '<A-j>', ss.resize_down, { desc = 'Resize down' })
+      vim.keymap.set('n', '<A-k>', ss.resize_up, { desc = 'Resize up' })
+      vim.keymap.set('n', '<A-l>', ss.resize_right, { desc = 'Resize right' })
+    end,
+  },
+  {
     'folke/flash.nvim',
     event = 'VeryLazy',
     opts = {
